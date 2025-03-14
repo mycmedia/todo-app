a4b6d697c88eaab012504c81d17d23ec38d637e2
mycmedia <34654283+mycmedia@users.noreply.github.com>
2025-03-14 18:36:57 -0400

    jwt

--- users-service/src/main/java/com/mycmedia/users_service/utils/JwtTokenUtil.java
+++ users-service/src/main/java/com/mycmedia/users_service/utils/JwtTokenUtil.java
@@ -3,35 +3,61 @@ package com.mycmedia.users_service.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.util.Date;
+import java.util.*;
+import java.security.Key;
+
 
 @Component
 public class JwtTokenUtil {
-    private final String secretKey = "f5a8074c32fecff1882b4f6384d407b5a8c2d418dad3832f7bd7f660ef137dc149a34fb0d1167532978e5e8f5d78424fc495af1cb33a2f831c0a6a4be72d67e1d90b76d322b53f129f7d5e8e62b521690cc815d0938871437c8d9d124295d336866b4501cc260452a7f8f6677b064f5d74f41fa0a00dfa7e6d1572ee4d63169b6f90a302934ecbb68eeb578453e212836df64ca07a61b336993b6eb182409daa8574354cb4dd00c32e81f44c089ace761c615807dd2d302696b840ebe48c547af0ae3c178d7b24895ecbcf1747bf6db2a4371ddbee53563f2f51bc4b8cc030b26be0c08cc0dcec03288634255807e99ab3140b5a3e0fef63d465fc11083448d8";
 
-    public String generateToken(String username) {
-        System.out.println("Generating token for user: " + username);
+    @Value("${security.jwt.secret}")
+    private String secretKey;
 
-        long validity = 3600000;
-
-
-        String compact = Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + validity))
-                .signWith(SignatureAlgorithm.HS256, secretKey) // Base64 encoding applied
-                .compact();
-
-        SecretKey key = Jwts.SIG.HS256.key().build();
-
-
-        System.out.println(compact);
-        return compact;
+    public String generateToken(String userName) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userName);
     }
+
+    private String createToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userName)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000* 60 * 60 *10))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+//    public String generateToken(String username) {
+//        System.out.println("Generating token for user: " + username);
+//
+//        long validity = 3600000;
+//
+//
+//        String compact = Jwts.builder()
+//                .setSubject(username)
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(System.currentTimeMillis() + validity))
+//                .signWith(SignatureAlgorithm.HS256, secretKey) // Base64 encoding applied
+//                .compact();
+//
+//        SecretKey key = Jwts.SIG.HS256.key().build();
+//
+//
+//        System.out.println(compact);
+//        return compact;
+//    }
 
     // Extract username from the token
     public String extractUsername(String token) {
@@ -49,13 +75,13 @@ public class JwtTokenUtil {
         T extract(Claims claims);
     }
 
-    // Extract all claims from the token
     private Claims extractAllClaims(String token) {
         return Jwts.parser()
+//                .verifyWith(getSignKey()) //verifyWith
                 .setSigningKey(secretKey)
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     // Validate if the token is expired
